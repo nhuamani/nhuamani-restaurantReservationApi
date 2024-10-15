1765c37c5618d9f1a41d2d13b3eaf1cac90534c8
nhuamani <nhuamani.c7@gmail.com>
2024-10-15 13:07:44 -0500

    first commit

---
+++ src/test/java/com/nhuamani/restaurantReservation/RestaurantReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhuamani.restaurantReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
