fd4452ab58d47e511a2c23eee7fbc715952eef21
nhuamani <nhuamani.c7@gmail.com>
2024-10-23 18:43:40 -0500

    refactoring

--- src/main/java/com/nhuamani/restaurantReservation/repository/DistrictRepository.java
+++ src/main/java/com/nhuamani/restaurantReservation/repository/DistrictRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface DistrictRespository extends JpaRepository<District, Long> {
+public interface DistrictRepository extends JpaRepository<District, Long> {
 
     Optional<District> findByName(String name);
 
--- src/main/java/com/nhuamani/restaurantReservation/service/DistrictService.java
+++ src/main/java/com/nhuamani/restaurantReservation/service/DistrictService.java
@@ -4,7 +4,7 @@ import com.nhuamani.restaurantReservation.domain.entity.District;
 import com.nhuamani.restaurantReservation.dto.response.DistrictResponseDTO;
 import com.nhuamani.restaurantReservation.exception.ResourceNotFoundException;
 import com.nhuamani.restaurantReservation.mapper.DistrictMapper;
-import com.nhuamani.restaurantReservation.repository.DistrictRespository;
+import com.nhuamani.restaurantReservation.repository.DistrictRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,17 +15,17 @@ import java.util.List;
 @Service
 public class DistrictService {
 
-    private final DistrictRespository districtRespository;
+    private final DistrictRepository districtRepository;
     private final DistrictMapper districtMapper;
 
     @Transactional(readOnly = true)
     public List<DistrictResponseDTO> getAllDistricts() {
-        List<District> districts = districtRespository.findAll();
+        List<District> districts = districtRepository.findAll();
         return districtMapper.toResponseDtoList(districts);
     }
 
     public DistrictResponseDTO getByIdDistrict(Long id) {
-        District district = districtRespository.findById(id).orElseThrow(() -> new ResourceNotFoundException("District not found with id: " + id));
+        District district = districtRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("District not found with id: " + id));
         return districtMapper.toResponseDto(district);
     }
 
